4cf06c0dfc2f7d419ed600ba15f9b363c1f3bd29
Cesar <161717056+CezinhaDev@users.noreply.github.com>
2024-08-01 13:19:14 -0300

    Add files via upload

---
+++ projeto.js/script.js
@@ -0,0 +1,25 @@
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var img  = window.document.getElementById('imagem')
+
+var data = new Date()
+//var hora = data.getHours()
+var hora = 22;
+
+msg.innerHTML = `Agora são ${hora} horas`
+
+if(hora >= 0 && hora <12){
+    /* BOM DIA */
+    img.src = 'manha.jpg'
+    document.body.style.background = '#e2cd9f'
+} else if(hora >= 12 && hora <= 18){
+    /*BOA TARDE*/
+    img.src = 'tarde.avif'
+    document.body.style.background = '#b9846f'
+} else{
+    /*BOA NOITE*/
+    img.src = 'noite.avif'
+    document.body.style.background = '#515154'
+}
+}
